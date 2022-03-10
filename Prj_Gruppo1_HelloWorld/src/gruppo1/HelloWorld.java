34ba5e874ad142a14dfd0b2cdc27170265febf79
Pietro Chiartano <chiartano.pietro@gmail.com>
2022-03-10 10:41:16 +0100

    Aggiunto Pietro

--- Prj_Gruppo1_HelloWorld/src/gruppo1/HelloWorld.java
+++ Prj_Gruppo1_HelloWorld/src/gruppo1/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		System.out.println("Ciao sono Andrea Cirillo");
 		System.out.println("Ciao sono Matteo Marziano");
 		System.out.println("Ciao sono Paolo Carena");
-		
+		System.out.println("Ciao sono Pietro Chiartano");
 
 	}
 
